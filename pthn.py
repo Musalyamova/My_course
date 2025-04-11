78b21a15f4aa8ab8ab94ccc47f2f93b2501d8515
Musalyamova <musalyamovaalina@gmail.com>
2025-04-12 01:06:45 +0500

    дз 1

---
+++ pthn.py
@@ -0,0 +1,33 @@
+# Задание 1
+# data = input('Введите дату: ')
+# task = input('Введите задачу: ')
+# print(data, task)
+
+# Задание 2
+# data1 = input('Введите дату: ')
+# task1 = input('Введите задачу: ')
+# data2 = input('Введите дату: ')
+# task2 = input('Введите задачу: ')
+# data3 = input('Введите дату: ')
+# task3 = input('Введите задачу: ')
+# print(data1, task1)
+# print(data2, task2)
+# print(data3, task3)
+
+# или можно решить след образом:
+
+# lst = []
+# for i in range(3):
+#     data = input('Введите дату: ')
+#     task = input('Введите задачу: ')
+#     lst.append(data + ' ' + task)
+# for i in lst:
+#     print(i)
+
+# Задача 3
+# dictionary = {}
+# for i in range(3):
+#     data = input('Введите дату: ')
+#     task = input('Введите задачу: ')
+#     dictionary[data] = task
+# print(dictionary) # можно вывести на экран
