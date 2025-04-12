85f2ef659cf6c20494c9959cb62adade7dfb780a
Musalyamova <musalyamovaalina@gmail.com>
2025-04-13 02:47:40 +0500

    Add files via upload
    вторая домашка

--- pthn.py
+++ pthn.py
@@ -1,33 +1,39 @@
-# Задание 1
-# data = input('Введите дату: ')
-# task = input('Введите задачу: ')
-# print(data, task)
+HELP = """
+help - напечатать справку по программе. 
+add - добавить задачу в список (название задачи
+запращиваем у пользователя).
+show - напечатать все добавленные задачи. 
+exit - выход из программы. """
 
-# Задание 2
-# data1 = input('Введите дату: ')
-# task1 = input('Введите задачу: ')
-# data2 = input('Введите дату: ')
-# task2 = input('Введите задачу: ')
-# data3 = input('Введите дату: ')
-# task3 = input('Введите задачу: ')
-# print(data1, task1)
-# print(data2, task2)
-# print(data3, task3)
+tasks = []
+today = []
+tomorrow = []
+other = []
 
-# или можно решить след образом:
+run = True
 
-# lst = []
-# for i in range(3):
-#     data = input('Введите дату: ')
-#     task = input('Введите задачу: ')
-#     lst.append(data + ' ' + task)
-# for i in lst:
-#     print(i)
+while run == True:
+    command = input("Введите команду: ")
+    if command == 'help':
+        print(HELP)
+    elif command == 'show':
+        print(tasks)
+    elif command == 'add':
+        task = input('Введите название задачи: ')
+        tasks.append(task)
+        data = input('Введите дату выполнения задачи: ')
+        if data == 'Сегодня':
+            today.append(task)
+        elif data == 'Завтра':
+            tomorrow.append(task)
+        elif data == 'Другой день':
+            other.append(task)
+        print('Задача добавлена')
+    elif command == 'exit':
+        print('Спасибо за использование!')
+        break
+    else:
+        print('Неизвестная команда')
+        break
 
-# Задача 3
-# dictionary = {}
-# for i in range(3):
-#     data = input('Введите дату: ')
-#     task = input('Введите задачу: ')
-#     dictionary[data] = task
-# print(dictionary) # можно вывести на экран
+print('До свидания!')
